02d853ab1958850fa741218902151b4f22084f30
James Bond <ns20092009@live.de>
2017-01-11 21:06:40 +0100

    Added HelloWorld.java with three loops and package-info.java

---
+++ src/main/java/com/egit/example/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.egit.example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		int i;
+		int j;
+		int k;
+
+		System.out.println("Hello World");
+
+		for (i = 0; i < 5; i++) {
+			System.out.println("Hello World once again with i");
+		}
+
+		for (j = 0; j < 5; j++) {
+			System.out.println("Hello World once again with j");
+		}
+
+		for (k = 0; k < 5; k++) {
+			System.out.println("Hello World, once again with k");
+		}
+
+	}
+
+}
---
+++ src/main/java/com/egit/example/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Marinchen
+ *
+ */
+package com.egit.example;
